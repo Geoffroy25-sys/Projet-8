b7188f660ffa91e2185fca0dbb86051ce55b9e38
Geoffroy25-sys <geoffroybiao@gmail.com>
2026-04-22 10:01:43 +0100

    Bloc 2 : Fondations et Workflow terminés

---
+++ fondations.py
@@ -0,0 +1,13 @@
+# Données du RPG
+joueur {
+    "nom": "Héros", 
+    "pv" : 100,
+    "attaque" : 30,
+    "défense" : 20,
+    "XP" : 10
+}
+monstre {
+    "nom" : "monstre",
+    "pv" : 50,
+    "attque" : 10
+}
